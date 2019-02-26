d964caf32ffe7ebac1805db697e6e4890f2e77e4
cmc373 <milinaire.c@gmail.com>
2019-02-26 13:29:33 +0100

    first_try

--- code/quickStart/quickStart.py
+++ code/quickStart/quickStart.py
@@ -1,3 +1,5 @@
 import gensim, logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+sentences = [['first', 'sentence'], ['second', 'sentence']]
+model = gensim .models.Word2vec(sentences, min_count=1)
