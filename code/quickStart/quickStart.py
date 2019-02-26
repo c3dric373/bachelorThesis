69ebf3f5780289789329a915216d608d019d97b7
Coedric Milinaire <milinair@darkstar.fim.uni-passau.de>
2019-02-26 14:11:52 +0100

    sentences builder with yield

--- code/quickStart/quickStart.py
+++ code/quickStart/quickStart.py
@@ -1,5 +1,31 @@
-import gensim, logging
+import gensim, logging, smart_open, os
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 sentences = [['first', 'sentence'], ['second', 'sentence']]
-model = gensim .models.Word2vec(sentences, min_count=1)
+model = gensim .models.Word2Vec(sentences, min_count=1)
+
+filenames=['./data/f1.txt', './data/f2.txt'] 
+
+for i,fname in enumerate(filenames):
+    with smart_open.smart_open(fname, 'w') as fout: 
+        for line in sentences[i]:
+            fout.write(line + '\n')
+
+class MySentences:
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __iter__(self):
+        for fname in os.listdir(self.dirname):
+            for line in smart_open.smart_open(os.path.join(self.dirname, fname), 'r'):
+                yield line.split()
+
+sentences = MySentences('./data/')
+print(list(sentences))
+
+model = gensim.models.Word2Vec(sentences, min_count=1)
+print(model)
+print(model.wv.vocab)
+
+
+
